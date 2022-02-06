051a9ff0a3f4d4e93f34c222a0398a040d075b3a
rsquinlan <ryansquinlan01@gmail.com>
2022-02-06 15:43:35 -0700

    build + package

--- server/index.js
+++ server/index.js
@@ -41,6 +41,15 @@ app.get("/", (req, res) => {
 //set port (5000 default, env file to specify)
 const port = process.env.PORT || 5000;
 
+// Accessing the path module
+const path = require("path");
+
+// Step 1:
+app.use(express.static(path.resolve(__dirname, "./client/build")));
+// Step 2:
+app.get("*", function (request, response) {
+  response.sendFile(path.resolve(__dirname, "./client/build"));
+});
 
 //launch server
 app.listen(port, () => {
